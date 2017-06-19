1b0cbb791cf325df7a89548fa45f84f202f6e3c3
Adam Grare <agrare@redhat.com>
2017-06-19 10:55:12 -0400

    Use standard inv persister from_yaml

--- persister.rb
+++ persister.rb
@@ -7,34 +7,16 @@ kafka = Kafka.new(
 
 consumer = kafka.consumer(group_id: "miq-persisters")
 consumer.subscribe("inventory")
-ems = ManageIQ::Providers::Openshift::ContainerManager.first
 
-def from_yaml(ems, inv_yaml)
-  inv_collections = {}
-  raw_collections = YAML.load(inv_yaml)
-
-  raw_collections.each do |collection|
-    init_attrs = collection.except(:data)
-
-    # fixup the inv collection
-    init_attrs[:parent]      = ems
-    init_attrs[:model_class] = init_attrs[:model_class].constantize
-
-    inv_collections[collection[:name]] = ManagerRefresh::InventoryCollection.new init_attrs
-  end
-
-  raw_collections.each do |collection|
-    inv_collections[collection[:name]].from_raw_data(collection[:data], inv_collections)
-  end
-
-  inv_collections
+def from_yaml(inv_yaml)
+  persister = ManagerRefresh::Inventory::Persister.from_yaml(inv_yaml)
+  return persister.manager, persister.collections
 end
 
 puts "Waiting for inventory"
 consumer.each_message do |message|
   begin
-    inv_collections = from_yaml(ems, message.value)
-
+    ems, inv_collections = from_yaml(message.value)
 
     puts "Saving Inventory..."
     ManagerRefresh::SaveInventory.save_inventory(ems, inv_collections.values)
