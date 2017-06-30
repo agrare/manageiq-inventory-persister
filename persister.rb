68504124ed4c4330ffd3325f5defcf929a21b17e
Adam Grare <agrare@redhat.com>
2017-06-30 10:56:43 -0400

    Move to activemq from kafka

--- persister.rb
+++ persister.rb
@@ -1,28 +1,23 @@
-require 'kafka'
-
-kafka = Kafka.new(
-    seed_brokers: ["localhost:9092"],
-    client_id: "miq-persister",
-)
-
-consumer = kafka.consumer(group_id: "miq-persisters")
-consumer.subscribe("inventory")
-
 def from_yaml(inv_yaml)
   persister = ManagerRefresh::Inventory::Persister.from_yaml(inv_yaml)
   return persister.manager, persister.collections
 end
 
 puts "Waiting for inventory"
-consumer.each_message do |message|
+client = ActiveMqClient.open(true)
+MiqQueue.subscribe_job(client, :service => 'ems_inventory') do |_sender, _mtype, message|
   begin
-    ems, inv_collections = from_yaml(message.value)
+    persister = ManagerRefresh::Inventory::Persister.from_raw_data(message)
 
     puts "Saving Inventory..."
-    ManagerRefresh::SaveInventory.save_inventory(ems, inv_collections.values)
+    ManagerRefresh::SaveInventory.save_inventory(persister.manager, persister.inventory_collections)
     puts "Save Inventory...Complete"
   rescue => err
     puts "#{err}"
     puts "#{err.backtrace.join("\n")}"
   end
+end
+
+loop do
+  sleep(1)
 end
